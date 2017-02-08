98aaf3eedb08c6f07f40d9aa33f9d7d773e1294b
Victoria Retallick <vretallick@zedge.net>
2017-02-08 21:28:08 +0100

    Add some hacky support for small screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,10 +11,10 @@
   function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
+    xobj.overrideMimeType('application/json');
     xobj.open('GET', 'countries.json', true);
     xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
+      if (xobj.readyState == 4 && xobj.status == '200') {
         // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
         callback(xobj.responseText);
       }
